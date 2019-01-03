9ef2cdc94904b19e841fbabb9e5766899201cb2a
Borman666 <Borman010884@yandex.ru>
2019-01-03 17:58:47 +0300

    HW-2

---
+++ Second Lesson/script.js
@@ -0,0 +1,24 @@
+// let x = 5; 
+// console.log(x++);
+
+// console.log([ ] + false - null + true); 
+
+// let y = 1; 
+// let x = y = 2; 
+// console.log(x);
+
+console.log([ ] + 1 + 2);
+
+console.log(2 && 1 && null && 0 && undefined);
+
+console.log(!!( a && b ));
+console.log(2 && 3 );
+
+a = [1, 2, 3];
+b = [1, 2, 3];
+console.log(a==b);
+
+console.log(+"Infinity");
+console.log("Ёжик" > "яблоко");
+
+console.log(0 || "" || 2 || undefined || true || falsе);
